bbb87f905ec5bf74dbd9fdd65caa478c8e0ea32a
Stefano Frassetto <frassetto.stefano@gmail.com>
2023-06-25 09:03:27 +0200

    Fix tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,4 +6,4 @@ def test_do_nothing() -> None:
     assert core.do_nothing() is None
 
 def test_some_untested_fn() -> None:
-    assert core.some_untested_fn is None
+    assert core.some_untested_fn() is None
